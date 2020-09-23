5dbff95e0a7955163a2d5f8701a7a5754a7fceb9
Chris Asakawa <casakawa@jamasoftware.com>
2020-09-23 14:27:59 -0700

    completed dev

---
+++ README.md
@@ -0,0 +1,83 @@
+# Item Flag Delete
+The purpose of this script is to update a list of items to be flagged delete. The list of items must be in a CSV format. and each 
+identifier must be unique.
+
+## Installation
+This section contains information on how to install the required dependencies for this script.
+
+### Pre-Requisites
+* [Python 3.7+](https://www.python.org/downloads/release/python-377/) If using pipenv you must use a python 3.7.X 
+version.  If installing requirements manually you may use any python version including 3.8+ however testing has only
+been done against python 3.7
+
+* [py-jama-rest-client](https://pypi.org/project/py-jama-rest-client/)
+
+* Enable the REST API on your Jama Connect instance
+
+## Pipenv installation (Recommended)
+If you do not already have Pipenv installed on your machine you can learn how to install it here: 
+[https://pypi.org/project/pipenv/](https://pypi.org/project/pipenv/)
+
+The required dependencies for this project are managed with Pipenv and can be installed by opening a terminal
+to the project directory and entering the following command:
+```bash
+pipenv install
+```
+
+## Manual installation
+If you do not wish to use Pipenv you may manually install the required dependencies with pip.
+```bash
+pip install --user py-jama-rest-client
+```
+
+## Usage
+This section contains information on configuration and execution the script.
+
+### Configuration
+Before you can execute the script you must configure the script via a config file.  The config file is
+structured in a standard .ini file format. there is an example config.ini file included with this repo that you
+may modify with your settings.  I recommend that you create a copy of the template config file and rename it to
+something that is meaningful for your execution.
+
+#### Client Settings:
+This section contains settings related to connecting to your Jama Connect REST API.
+
+* `jama_connect_url`: this is the URL to your Jama Connect instance
+
+* `oauth`: setting this value to 'false' will instruct the client to authenticate via basic authentication.  Setting this 
+value to 'true' instructs the client to use OAuth authentication protocols
+
+* `user_id`: This should be either your username or clientID if using OAuth
+
+* `user_secret`: This should be either your password or client_secret if using OAuth
+
+#### Script PARAMETERS:
+This section contains settings required to run the script
+* `csv_file_path`: (string) relative path to the source csv file
+* `csv_using_header`: (boolean) flag for if csv is using a header row
+* `csv_header_value`: (string) the column header of the identifier column in the CSV file, this is required 
+if `csv_using_header` is set to true.
+
+* `deleted_flag_field_map`: (json map) inline json map of item type id -> field name. every item type included in the source file
+must be included into this map. (e.g. `csv_using_header = {"114": "deleted", "27": "is_deleted"}`) 
+
+
+
+
+
+## Running the script
+
+1) Open a terminal to the project directory.
+2) If using pipenv enter the following(otherwise skip to step 3):
+   ```bash
+   pipenv shell 
+   ``` 
+3) Enter the following command into your terminal (Note that the script accepts one parameter and that is the path to
+the config file created above):  
+   ```bash 
+   python flag_delete.py
+   ```
+
+## Output
+Execution logs will be output to the terminal as well as output to a log file in the logs/ folder located next to the 
+script.
---
+++ flag_delete.py
@@ -0,0 +1,229 @@
+import configparser
+import csv
+import datetime
+import json
+import logging
+import os
+import sys
+
+from py_jama_rest_client.client import JamaClient, APIException
+
+logger = logging.getLogger(__name__)
+
+
+def init_logging():
+    try:
+        os.makedirs('logs')
+    except FileExistsError:
+        pass
+    current_date_time = datetime.datetime.now().strftime("%Y-%m-%d %H_%M_%S")
+    log_file = 'logs/dox_importer_' + str(current_date_time) + '.log'
+    logging.basicConfig(filename=log_file, level=logging.INFO)
+    logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
+
+
+def parse_config():
+    # allow the user to shorthand this and just look for the 'config.ini' file
+    if len(sys.argv) == 1:
+        current_dir = os.path.dirname(__file__)
+        path_to_config = 'config.ini'
+        if not os.path.isabs(path_to_config):
+            path_to_config = os.path.join(current_dir, path_to_config)
+
+    # use the config file location
+    if len(sys.argv) == 2:
+        current_dir = os.path.dirname(__file__)
+        path_to_config = sys.argv[1]
+        if not os.path.isabs(path_to_config):
+            path_to_config = os.path.join(current_dir, path_to_config)
+
+    # Parse config file.
+    configuration = configparser.ConfigParser()
+    try:
+        with open(path_to_config, encoding="utf8", errors='ignore') as file:
+            configuration.read_file(file)
+    except Exception as e:
+        logger.error("Unable to parse configuration file. exception: " + str(e))
+        exit(1)
+
+    return configuration
+
+
+def create_jama_client(config: configparser.ConfigParser):
+    global instance_url
+    url = None
+    user_id = None
+    user_secret = None
+    oauth = None
+    try:
+        url = config.get('CLIENT_SETTINGS', 'jama_connect_url').strip()
+        # Clenup URL field
+        while url.endswith('/') and url != 'https://' and url != 'http://':
+            url = url[0:len(url) - 1]
+        # If http or https method not specified in the url then add it now.
+        if not (url.startswith('https://') or url.startswith('http://')):
+            url = 'https://' + url
+        oauth = config.getboolean('CLIENT_SETTINGS', 'oauth')
+        user_id = config.get('CLIENT_SETTINGS', 'user_id').strip()
+        user_secret = config.get('CLIENT_SETTINGS', 'user_secret').strip()
+        instance_url = url
+    except configparser.Error as config_error:
+        logger.error("Unable to parse CLIENT_SETTINGS from config file because: {}, "
+                     "Please check config file for errors and try again."
+                     .format(str(config_error)))
+        exit(1)
+
+    return JamaClient(url, (user_id, user_secret), oauth=oauth)
+
+
+def process_csv():
+    csv_content = []
+    csv_lines_read = 0
+
+    # get the CSV script params from the config file
+    csv_file = ''
+    csv_using_header = True
+    csv_header_value = ''
+    try:
+        csv_file = conf['SCRIPT_PARAMETERS']['csv_file_path']
+        csv_using_header = conf.getboolean('SCRIPT_PARAMETERS', 'csv_using_header')
+        if csv_using_header:
+            csv_header_value = conf.get('SCRIPT_PARAMETERS', 'csv_header_value').strip()
+    except Exception as e:
+        logger.error(
+            'unable to retrieve required script parameters csv_using_header/csv_header_value. e:{}'.format(str(e)))
+        exit(1)
+
+    # validate that we have the required script settings
+    if csv_using_header and (csv_header_value is None or csv_header_value == ''):
+        logger.error('missing required script param csv_header_value')
+        exit(1)
+    if csv_file is None or csv_file == '':
+        logger.error('missing required script param csv_file_path')
+        exit(1)
+
+    # Open the CSV file for reading, use the utf-8-sig encoding to deal with excel file type outputs.
+    with open(str(csv_file), encoding='utf-8-sig') as open_csv_file:
+        # We need to get a dict reader, if the CSV file has headers, we dont need to supply them
+        csv_dict_reader = csv.DictReader(open_csv_file)
+
+        # validate that we have a valid username column
+        if csv_using_header and csv_header_value not in csv_dict_reader.fieldnames:
+            logger.error('unable to find the identifier column{} in the CSV file. script exiting...')
+            exit(1)
+
+        # Begin processing the data in the CSV file.
+        for row_number, row_data in enumerate(csv_dict_reader):
+            # For each row in the CSV file we will append an object to a list for later processing.
+            # First get source and target data. These are mandatory, a missing data point here is an error.
+            csv_lines_read += 1
+
+            # run some quick validations that we have data in the cells
+            current_row_rel_data = {
+                'row': row_number + 1 if csv_using_header else row_number,
+                'id': row_data[csv_header_value] if csv_using_header else json.dumps(row_data),
+            }
+            csv_content.append(current_row_rel_data)
+
+    logger.info('Successfully processed {} CSV rows. from file {}'.format(str(csv_lines_read), csv_file))
+    return csv_content
+
+
+if __name__ == "__main__":
+    # Setup logging
+    init_logging()
+
+    # Get Config File Path
+    conf = parse_config()
+
+    # Create Jama Client
+    jama_client = create_jama_client(conf)
+
+    # get the delete flag field map from the config
+    deleted_flag_field_map = {}
+    try:
+        json_string = conf.get('SCRIPT_PARAMETERS', 'deleted_flag_field_map')
+        deleted_flag_field_map = json.loads(json_string)
+    except Exception as e:
+        logger.error('unable to parse out deleted_flag_field_map from the config.ini, e:{}'.format(str(e)))
+
+    # process the csv content
+    csv_items = process_csv()
+    item_list = []
+    fetch_counter = 0
+    for csv_item in csv_items:
+        fetch_counter += 1
+        fetched_items = []
+        try:
+            logger.info('{}/{} processing item with identifier:{} ...'.format(fetch_counter, len(csv_items),
+                                                                                         csv_item.get('id')))
+            fetched_items = jama_client.get_abstract_items(contains=csv_item.get('id'))
+        except APIException as e:
+            logger.error(
+                '    ERROR unable to retrieve items for csv entry: {} with error: {}, skipping item...'.format(str(csv_item),
+                                                                                                     str(e)))
+            break
+
+        # we MUST have only one match here to continue
+        if len(fetched_items) == 0:
+            logger.error('    ERROR found zero matches for csv entry: {}. skipping item...'.format(str(csv_item)))
+            break
+        # more than one match found
+        elif len(fetched_items) > 1:
+            logger.error('   ERROR found multiple matches for csv entry: {}. skipping item...'.format(str(csv_item)))
+            break
+        # otherwise this exactly one match found
+        else:
+            # save this item to the item list
+            item_list.append(fetched_items[0])
+            logger.info('    found match to corresponding Jama ID: {}'.format(fetched_items[0].get('id')))
+
+    # loop over the item list and check to see if we need to update the item
+    # this work might already be done.
+    update_list = []
+    item_counter = 0
+    for item in item_list:
+        item_counter += 1
+        # get the field from the delete field map
+        item_type_id = str(item.get('itemType'))
+        # validate that the mapping has the current item type
+        if item_type_id not in deleted_flag_field_map:
+            logger.error(
+                'itemtype ID:{} is missing from the config.ini deleted_flag_field_map. skipping item...'.format(
+                    item_type_id))
+            break
+
+        # format the delete field to be: fieldName $ itemTypeId (custom field)
+        delete_field_name = '{}${}'.format(deleted_flag_field_map.get(item_type_id), item_type_id)
+
+        # validate that the fields payload has the delete field
+        if delete_field_name not in item.get('fields'):
+            logger.error('item does not have the field: {}. skipping item...'.format(delete_field_name))
+            break
+
+        # we only need to update this item if its currently set to False.
+        if not item.get('fields').get(delete_field_name):
+            update_list.append((item.get('id'), delete_field_name))
+        else:
+            logger.info('item ID:{} already flagged for delete skipping item...'.format(item.get('id')))
+
+    logger.info('Identified {} items to be flagged for delete'.format(len(update_list)))
+    update_counter = 0
+
+    # iterate over the update list and update each item
+    for update_item in update_list:
+        try:
+            update_counter += 1
+            patch_payload = {
+                'op': 'replace',
+                'path': '/fields/' + update_item[1],
+                'value': True
+            }
+            jama_client.patch_item(update_item[0], patch_payload)
+            logger.info('item {}/{} - successfully updated item id:{} to be flagged for delete'.format(update_counter,
+                                                                                                       len(update_list),
+                                                                                                       update_item[0]))
+        except APIException as e:
+            logger.error('unable to update item ID:{} exception:{}'.format(update_item[0], str(e)))
+
+    logger.info('Script Complete')
